8ffbc7fa83371f85cd8c97a815159d4675c70347
algoritmist <91695266+algoritmist@users.noreply.github.com>
2023-12-25 13:01:47 +0000

    feat: implement cpu emulator without tests

--- src/Emulator.hs
+++ src/Emulator.hs
@@ -1,134 +1,114 @@
 module Emulator where
-import Data.Map as Map
-import ISA
+import           Data.Map as Map
+import           ISA
 
-MAX_DMEM = 4096
-MAX_IMEM = 4096
-MAX_INMEM = 16
-MAX_OUTMEM = 16
+maxDMem = 4096
+maxIMem = 4096
+maxInMem = 16
+maxOutMem = 16
+
+invalidRegister = Left "Error: invalid register"
 
 data CPU = CPU{
-    registers :: Map Register Int,
-    instructionMemory :: Map Instruction Int,
-    dataMemory :: Map Int Int,
-    inMemory :: Map Int Int,
-    outMemory :: Map Int Int
+    regs   :: Map Register Int,
+    iMem   :: Map Int Instruction,
+    dMem   :: Map Int Int,
+    inMem  :: Map Int Int,
+    outMem :: Map Int Int
     } deriving(Show)
 
+
+
 initDefault :: CPU
-initDefault = 
+initDefault =
     let
-        regs = Map.fromList [("zero", 0), ("ra", 0), ("a0", 0), ("a1", 0), ("t0", 0), ("t1", 0), ("t2", 0), ("s0", 0), ("s1", 0), ("s2", 0), ("pc", 0)]
-        iMem = replicate MAX_IMEM 0
-        dMem = replicate MAX_DMEM 0
-        inMem = replicate  MAX_INMEM 0
-        outMem = replicate MAX_OUTMEM 0
+        generator = Prelude.map (, 0) (iterate (+ 1) 0)
+        regs = Map.fromList $ Prelude.map (, 0) registers
+        iMem = Map.fromList $ Prelude.take maxIMem $  Prelude.map (, Nop) (iterate (+ 1) 0)
+        dMem = Map.fromList  $ Prelude.take maxDMem generator
+        inMem = Map.fromList $ Prelude.take maxInMem generator
+        outMem = Map.fromList $ Prelude.take maxOutMem generator
     in CPU regs iMem dMem inMem outMem
 
-getPC :: CPU -> Register
-getPC CPU{regs} = regs ! "pc"
-
 emulate :: CPU -> IO ()
-emulate cpu = do
-    pc <- regs ! pc
-    instruction <- iMem ! pc
-    result <- execute cpu instruction
+emulate cpu@CPU{regs, iMem} = do
+    let pc = regs ! ISA.pc
+    let instruction = iMem ! pc
+    let result = execute cpu instruction
     case result of
-        Left err -> err
+        Left err -> fail err
         Right cpu' -> do
-            cpu'' <- postExecute cpu'
-            show cpu''
+            let cpu'' = postExecute cpu'
+            print cpu''
             emulate cpu''
 
 postExecute :: CPU -> CPU
 postExecute cpu@(CPU regs iMem dMem inMem outMem) =
     let
-        pc = getPC cpu
-        regs' = Map.insert "pc" (pc + 4) regs
+        pc = regs ! ISA.pc
+        regs' = Map.insert ISA.pc (pc + 4) regs
     in
         CPU regs' iMem dMem inMem outMem
-    
+
 execute :: CPU -> Instruction -> Either String CPU
-execute (CPU regs iMem dMem inMem outMem) operator@(MathOp rd rs1 rs2 _ opcode) = do
-    funct <- case operator of
-        Add {}-> Right (+)
-        Sub {} -> Right (-)
-        Mul {} -> Right (*)
-        Div {} -> Right (/)
+execute (CPU regs iMem dMem inMem outMem) operator@(MathOp opcode rd rs1 rs2 _) = do
+    op <- case opcode of
+        0 -> Right (+)
+        1 -> Right (-)
+        2 -> Right (*)
+        3 -> Right Prelude.div
         _ -> Left $ "Error: No math operation with opcode " ++ show opcode
-    case funct of 
-        Left err -> Left err
-        Right op -> do
-            alu <- op <$> findKey rs1 regs <*> findKey rs2 regs
-            case alu of
-                Nothing -> Left "Error: Invalid register"
-                Just value -> do
-                    regs' <- Map.insert rd alu regs
-                    Right $ CPU regs' iMem dMem inMem outMem
+    let alu = (regs ! rs1) `op` (regs ! rs2)
+    let regs' = Map.insert rd alu regs
+    Right $ CPU regs' iMem dMem inMem outMem
 
-execute cpu@(CPU regs iMem dMem inMem outMem) branch@(Branch rs1 rs2 imm opcode) = do
-    funct <- case branch of
-        JE {} -> Right (==)
-        JNE {} -> Right (=/)
-        JG {} -> Right (>)
-        JL {} -> Right (<)
-        _ -> Left $ "Error: No branch instruction with opcode " ++ opcode
-    case funct of
-        Left err -> Left err
-        Right op -> do
-            alu <- op <$> findKey rs1 regs <*> findKey rs2 regs
-            case alu of
-                Nothing -> Left: "Error: invalid register"
-                Just False -> Right cpu
-                Just True -> do
-                    pc <- getPC cpu
-                    regs' <- Map.insert "pc" (pc + imm) regs
-                    Right $ CPU regs' iMem dMem inMem outMem
+execute cpu@(CPU regs iMem dMem inMem outMem) branch@(Branch opcode _ rs1 rs2 imm) = do
+    op <- case opcode of
+        4 -> Right (==)
+        5 -> Right (/=)
+        6 -> Right (>)
+        7 -> Right (<)
+        _ -> Left $ "Error: No branch instruction with opcode " ++ show opcode
+    let alu = (regs ! rs1) `op` (regs ! rs2)
+    let pc = regs ! ISA.pc
+    let regs' = Map.insert ISA.pc (pc + imm) regs
+    Right $ CPU regs' iMem dMem inMem outMem
 
-execute cpu@(CPU regs iMem dMem inMem outMem) oparator@(MathImmideate rd rs1 imm opcode) = do
-    funct <- case operator of
-        AddI {}-> Right (+)
-        SubI {} -> Right (-)
-        MulI {} -> Right (*)
-        DivI {} -> Right (/)
-        _ -> Left $ "Error: No math operation with opcode " ++ show opcode
-    case funct of 
-        Left err -> Left err
-        Right op -> do
-            alu <- op <$> findKey rs1 regs <*> imm
-            case alu of
-                Nothing -> Left "Error: Invalid register"
-                Just value -> do
-                    regs' <- Map.insert rd alu regs
-                    Right $ CPU regs' iMem dMem inMem outMem
+execute cpu@(CPU regs iMem dMem inMem outMem) oparator@(MathImmideate opcode rd rs1 imm) = do
+    op <- case opcode of
+        16 -> Right (+)
+        17 -> Right (-)
+        18 -> Right (*)
+        19 -> Right Prelude.div
+        _  -> Left $ "Error: No math operation with opcode " ++ show opcode
 
-execute cpu@(CPU regs iMem dMem inMem outMem) operation@(RegisterMemory rd rs1 imm opcode) = do
-    alu <- (+) <$> findKey rs1 regs <*> imm
-    case alu of
-        Nothing -> Left "Error: Invalid register"
-        Just addr -> do 
-            case operation of
-                LWM {} -> do
-                    dValue <- dMem ! addr
-                    regs' <- Map.insert rd dValue regs
-                    Right $ CPU regs' iMem dMem inMem outMem
-                SWM {} -> do
-                    rValue <- regs ! rd 
-                    dMem' <- Map.insert addr rValue dMem
-                    Right $ CPU regs iMem dMem' inMem outMem  
-        
-execute cpu@(CPU regs iMem dMem inMem outMem) operation@(MemoryMemory rd rs1 imm opcode) = do
-    rAddr <- regs ! rd
-    alu <- (+) <$> findKey rs1 regs <*> imm
-    case alu of
-        Nothing -> Left "Error: Invalid register"
-        Just addr -> do
-            case operation of
-                LWI {} -> do
-                    inValue <- inMem ! addr
-                    dMem' <- Map.insert rAddr inValue dMem
-                    Right $ CPU regs iMem dMem' inMem outMem  
-                SWO {} -> do
-                    outValue <- dMem ! addr
-                    outMem' <- Map.insert rAddr outValue outMem
-                    Right $ CPU regs iMem dMem inMem outMem'
+    let alu = (regs ! rs1) `op` imm
+    let regs' = Map.insert rd alu regs
+    Right $ CPU regs' iMem dMem inMem outMem
+
+execute cpu@(CPU regs iMem dMem inMem outMem) operation@(RegisterMemory opcode rd rs1 imm) = do
+    let addr = regs ! rs1 + imm
+    case opcode of
+        20 -> do
+            let dValue = dMem ! addr
+            let regs' = Map.insert rd dValue regs
+            Right $ CPU regs' iMem dMem inMem outMem
+        21 -> do
+            let rValue = regs ! rd
+            let dMem' = Map.insert addr rValue dMem
+            Right $ CPU regs iMem dMem' inMem outMem
+        _ -> Left $ "Invalid operation with opcode" ++ show opcode
+
+execute cpu@(CPU regs iMem dMem inMem outMem) operation@(MemoryMemory opcode rd rs1 imm) = do
+    let rAddr = regs ! rd
+    let addr = regs ! rs1 + imm
+    case opcode of
+        22 -> do
+            let inValue = inMem ! addr
+            let dMem' = Map.insert rAddr inValue dMem
+            Right $ CPU regs iMem dMem' inMem outMem
+        23 -> do
+            let outValue = dMem ! addr
+            let outMem' = Map.insert rAddr outValue outMem
+            Right $ CPU regs iMem dMem inMem outMem'
+        _ -> Left $ "Invalid operation with opcode" ++ show opcode
--- src/ISA.hs
+++ src/ISA.hs
@@ -1,67 +1,88 @@
 module ISA where
 
-type Register = Int
-zero = "x0"
-ra = "x1"
-pc = "x31"
+type Name = String
 
-type Offset = Int
+data Register =
+    Zero Name |
+    Argument Name |
+    Saved Name |
+    Temprorary Name |
+    Special Name deriving(Show, Ord, Eq)
+
+zero = Zero "x0"
+ra = Special "ra"
+pc = Special "pc"
+sp = Special "sp"
+a0 = Argument "a0"
+a1 = Argument "a1"
+a2 = Argument "a2"
+s0 = Saved "s0"
+s1 = Saved "s1"
+s2 = Saved "s2"
+t0 = Saved "t0"
+t1 = Saved "t1"
+t2 = Saved "t2"
+
+registers :: [Register]
+registers = [zero, ra, pc, sp, a0, a1, a2, s0, s1, s2, t0, t1, t2]
+
+
+type Opcode = Int
+type Rd = Register
+type Rs1 = Register
+type Rs2 = Register
+type Imm = Int
 
 {-
 I-type: imm - 12 bit signed, rs2 - 5 bit, rs1 - 5 bit, rd - 5 bit, opcode - 5 bit
 II-tpye: imm - 17 bit signed, rs1 - 5 bit, rd - 5 bit, opcode - 5 bit
 III-type: imm - 27 bit signed, opcode - 5 bit
 -}
+
 data Instruction =
-    MathOp    {imm :: Int, rs2 :: Register, rs1 :: Register, rd :: Register, opcode :: Int} |
-    Branch    {imm :: Int, rs2 :: Register, rs1 :: Register, rd :: Register, opcode :: Int} |
-    RegisterMemory {imm :: Int, rs1 :: Register, rd :: Register, opcode :: Int} |
-    MemoryMemory {imm :: Int, rs1 :: Register, rd :: Register, opcode :: Int} |
-    MathImmideate {imm :: Int, rs1 :: Register, rd :: Register, opcode :: Int} |
-    Jump {imm :: Int, opcode :: Int}
-
-valid :: Instruction -> Bool
-valid (T1 imm rs2 rs1 rd opcode) =
-    (-2^11 <= imm && imm < 2^11) && (0 <= rs2 && rs2 <= 31) && (0 <= rs1 && rs1 <= 31) &&
-    (0 <= rd && rd < opcode) && (0 <= opcode && opcode < 2^4)
-
-valid (T2 imm rs2 rs1 opcode) =
-    (-2^16 <= imm && imm < 2^16) && (0 <= rs2 && rs2 <= 31) && (0 <= rs1 && rs1 <= 31) &&
-    (2^4 <= opcode && opcode < 2^5 -2)
-
-valid (T3 imm rs2 rs1 opcode) = (-2^26 <= imm && imm < 2^27) && (opcode == 2^5-1)
+    MathOp Opcode Rd Rs1 Rs2 Imm |
+    Branch    Opcode Rd Rs1 Rs2 Imm |
+    RegisterMemory Opcode Rd Rs1 Imm |
+    MemoryMemory Opcode Rd Rs1 Imm |
+    MathImmideate Opcode Rd Rs1 Imm |
+    Jump Opcode Rd Imm |
+    Nop
+    deriving(Show)
 
 -- R-R instructions
-type Add rd rs1 rs2 = MathOp 0 rd rs1 rs2 0
-type Sub rd rs1 rs2 = MathOp 1 rd rs1 rs2 0
-type Mul rd rs1 rs2 = MathOp 2 rs2 rs1 rd 0
-type Div rd rs1 rs2 = MathOp 3 rs2 rs1 rd 0
+add rd rs1 rs2 = MathOp 0 rd rs1 rs2 0
+sub rd rs1 rs2 = MathOp 1 rd rs1 rs2 0
+mul rd rs1 rs2 = MathOp 2 rd rs1 rs2 0
+div rd rs1 rs2 = MathOp 3 rd rs1 rs2 0
 -- Branch instructions
-type JE rs1 rs2 imm = Branch 4 pc rs1 rs2 4 -- if(@rs1 == @rs2) pc <- pc + imm
-type JG rs1 rs2 imm = Branch 5 pc rs1 rs2 imm
-type JNE rs1 rs2 imm = Branch 6 pc rs1 rs2 imm
-type JL rs1 rs2 imm = Branch 7 pc rs1 rs2 imm
-type Ret = Branch 8 pc ra zero 0 -- pc <- ra + 4, pop ra
-type Jmp imm = Jump imm 30
+je = Branch 4 pc -- if(@rs1 == @rs2) pc <- pc + imm
+jne = Branch 5 pc
+jg = Branch 6 pc
+jl = Branch 7 pc
+jmp = Jump 8
 
 -- R-I instructions
-type AddI rd rs1 imm = MathImmideate rd rs1 imm 16
-type SubI rd rs1 imm = MathImmideate rd rs1 imm 17
-type MulI rd rs1 imm = MathImmideate rd rs1 imm 18
-type DivI rd rs1 imm = MathImmideate rd rs1 imm 19
+addI = MathImmideate 16
+subI = MathImmideate 17
+mulI = MathImmideate 18
+divI = MathImmideate 19
 
 -- R-M instructions
-type LWM rd rs1 imm = RegisterMemory rd rs1 imm 20
-type SWM rd rs1 imm = RegisterMemory rd rs1 imm 21
+lwm = RegisterMemory 20
+swm = RegisterMemory 21
 
 -- M-M instructions
-type LWI rd rs1 imm = MemoryMemory rd rs1 imm 22
-type SWO rd rs1 imm = MemoryMemory rd rs1 imm 23
+lwi = MemoryMemory 22
+swo = MemoryMemory 23
 
 -- Pseudo instructions
 push :: Register -> [Instruction]
-push rs = [Add pc pc 1, SWM pc rs 0]
+push rs = [addI sp sp 4, swm sp rs 0]
 pop :: Register -> [Instruction]
-pop rd = [LWM rd pc 0, Sub pc pc 1]
-call :: Offset -> [Instruction]
-call imm = Add ra pc zero : push ra ++ [Jmp imm]
+pop rd = [lwm rd sp 0, subI sp sp 4]
+ret :: [Instruction]
+ret =  jmp ra 0: pop pc  -- pc <- ra + 4, pop ra
+
+type Offset = Int
+call :: Register -> Offset -> [Instruction]
+call rd imm = add ra pc zero : push ra ++ [jmp rd imm]
